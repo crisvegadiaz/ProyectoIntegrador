c6d842b4d8b18cf9e8148ff8ed529e49b67e741a
crisvegadiaz <alexander85vega@gmail.com>
2023-11-19 12:48:37 -0300

    Ejercicio y el diseño diagrama Entidad-Relación

--- src/main/java/Inicio/ProyectoIntegrador.java
+++ src/main/java/Inicio/ProyectoIntegrador.java
@@ -3,6 +3,6 @@ package Inicio;
 public class ProyectoIntegrador {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Proyecto Integrador");
     }
 }
